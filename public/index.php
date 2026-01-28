5fd0598415f2cf05834b2f91716e4ed36d04ba71
Vido-Faresky <vidogamingmax@gmail.com>
2026-01-28 10:15:57 +0700

    wip: implement router app

--- public/index.php
+++ public/index.php
@@ -6,6 +6,6 @@
     $router->add('GET', '/students', 'StudentController', 'index');
     $router->add('GET', '/students/create', 'StudentController', 'create');
     $router->add('GET', '/students/{id}', 'StudentController', 'show');
-    $router->run();a
+    $router->run();
 
 ?>
