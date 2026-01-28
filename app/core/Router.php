7c31e9ed6d09659a88b8aaa1c050d1657bff02c5
Vido-Faresky <vidogamingmax@gmail.com>
2026-01-28 10:15:09 +0700

    feat: implement simple router

--- app/core/Router.php
+++ app/core/Router.php
@@ -5,13 +5,52 @@ use App\Controller\StudentController;
 
 class Router
 {
+    private array $routes = [];
 
+        public function add(string $method, string $uri, string $controller, string $function)
+        {
+            $this->routes[] = [
+                'method' => $method,
+                'uri' => $uri,
+                'controller' => $controller,
+                'function' => $function,
+            ];
+        }
+
+        private function buildPattern(string $uri)
+        {
+            $pattern = str_replace(
+                '{$id}',
+                '([0-9]+)',
+                $uri,
+            );
+
+             return '#^' . $pattern . '$#';
+        }
+
+       
+        
     public function run()
     {
         $method = $_SERVER['REQUEST_METHOD'];
         $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-        echo "{$method} {$uri}";
+        foreach ($this->routes as $route) {
+            $pattern = $this->buildPattern($route['path']);
+
+            if (preg_match($pattern, $uri, $matches)) {
+                array_shift($matches);
+                require_once './app/controllers/' . $route['controller'] . '.php';
+                $function = $route['function'];
+
+                $controllerClass = 'App\\Controllers\\' . $route['controller'];
+                $controller = new $controllerClass();
+
+                call_user_func_array([$controller, $function], $matches);
+
+                return;
+            }
+        }
 
         if ($method == 'GET' && $uri == '/students') {
             require_once './app/controllers/StudentController.php';
--- public/index.php
+++ public/index.php
@@ -3,9 +3,9 @@
     use App\Core\Router;
 
     $router = new Router();
-    // $router->add('GET', '/students', 'StudentController', 'index');
-    // $router->add('GET', '/students/create', 'StudentController', 'create');
-    // $router->add('GET', '/students/{id}', 'StudentController', 'show');
+    $router->add('GET', '/students', 'StudentController', 'index');
+    $router->add('GET', '/students/create', 'StudentController', 'create');
+    $router->add('GET', '/students/{id}', 'StudentController', 'show');
     $router->run();
 
 ?>
